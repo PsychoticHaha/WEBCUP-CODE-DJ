c214e214d540b0111b9dc6e8de28101937ef3e08
Onjaniaina754 <o.rabenirina@fulldigits.com>
2025-05-17 14:54:10 +0300

    feat: implementation openai

---
+++ pages/api/openai/emotion.ts
@@ -0,0 +1,37 @@
+import openai from "@/lib/openai";
+import { s } from "motion/react-client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+export default async function generateEmotion(
+    req: NextApiRequest,
+    res: NextApiResponse,
+) {
+    if (req.method !== "POST") {
+        return res.status(405).json({ error: "Method not allowed" });
+    }
+    const message = req.body.message
+    if (!process.env.OPEN_AI_SECRET_KEY) {
+        return res.status(500).json({ error: "OpenAI API key not configured",succes: false });
+    }
+    const result = await openai.chat.completions.create({
+        model: "gpt-4o-mini",
+        messages: [
+            {
+                role: "user",
+                content: message,
+            },
+        ],
+    })
+    if (!result.choices || result.choices.length === 0) {
+        return res.status(500).json({ message: "No choices returned from OpenAI", code: 'not.message',success: true });
+    }
+    const responseContent = result.choices[0].message.content;
+    if (!responseContent) {
+        return res.status(500).json({ error: "No content in the response",success: false });
+    }
+    res.status(200).json({
+        message: responseContent,
+        success: true,
+    });
+}
