90b9f7f949996b6e2e13abbcfdae88ce0cca8a03
Fanasina Ramalandimanana <fanasina@fulldigits.com>
2025-05-17 22:50:57 +0300

    chore: clean up default content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
 import HeroSection from "@/components/hero-section";
-import Features from "@/components/features-1";
-import ContentSection from "@/components/content-1";
-import CallToAction from "@/components/call-to-action";
 import FooterSection from "@/components/footer";
 import { useEmotion } from "@/hooks/useEmotion";
 
@@ -11,9 +8,6 @@ export default function Home() {
     <>
       <h1>Emotion: {message}</h1>
       <HeroSection />
-      <Features />
-      <ContentSection />
-      <CallToAction />
       <FooterSection />
     </>
   );
