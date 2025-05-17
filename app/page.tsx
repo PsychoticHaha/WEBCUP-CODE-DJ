676c0eb18733271908206c51dc857aba5a9d57d2
LovaxCoding <lnantenaina78@gmail.com>
2025-05-17 15:52:08 +0300

    hero section done

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 import HeroSection from "@/components/hero-section";
-import Features from "@/components/features-1";
-import ContentSection from "@/components/content-1";
-import CallToAction from "@/components/call-to-action";
-import FooterSection from "@/components/footer";
+// import Features from "@/components/features-1";
+// import ContentSection from "@/components/content-1";
+// import CallToAction from "@/components/call-to-action";
+// import FooterSection from "@/components/footer";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-      <Features />
+      {/* <Features />
       <ContentSection />
       <CallToAction />
-      <FooterSection />
+      <FooterSection /> */}
     </>
   );
 }
