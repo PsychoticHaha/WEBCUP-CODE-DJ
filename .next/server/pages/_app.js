d8f659325ff3e9ef5c34f251143412add9edaf6c
Fanasina Ramalandimanana <fanasina@fulldigits.com>
2025-05-17 22:19:14 +0300

    chore: remove build bundles indexation

--- .next/server/pages/_app.js
+++
@@ -1,6 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/pages/_app.js";
-const runtime = require("../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_48441a4e._.js");
-runtime.loadChunk("server/chunks/ssr/[root of the server]__c2e50c18._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/pages/_app.tsx [ssr] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/pages/_app.tsx [ssr] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"lob74Fa3p44U7Ny8WPZsgHJqJ2sE4enfHiupfjGHifI=\"\n}"
