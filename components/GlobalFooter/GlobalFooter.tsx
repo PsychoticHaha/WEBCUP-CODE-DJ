860624157732450de2ca67e95c06ebe0f00995fc
Fanasina Ramalandimanana <fanasina@fulldigits.com>
2025-05-17 23:20:11 +0300

    feat: make a global content

---
+++ components/GlobalFooter/GlobalFooter.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+
+export const GlobalFooter = () => {
+  return (
+    <Box sx={{ minHeight: "100px", backgroundColor: "red" }}>GlobalFooter</Box>
+  );
+};
---
+++ styles/breakpoints.ts
@@ -0,0 +1,12 @@
+export const breakpoints = {
+  values: {
+    xs: 0,
+    sm: 576,
+    md: 768,
+    mdx: 920,
+    lg: 1024,
+    xl: 1200,
+    mxl: 1310,
+    hi: 1536,
+  },
+};
