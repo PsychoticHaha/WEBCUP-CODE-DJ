dc26f9a6299e9b12995c0d23c124c05fd53e0fc4
Onjaniaina754 <o.rabenirina@fulldigits.com>
2025-05-17 14:54:10 +0300

    feat: implementation openai

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,11 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
 import HeroSection from "@/components/hero-section";
 import Features from "@/components/features-1";
 import ContentSection from "@/components/content-1";
 import CallToAction from "@/components/call-to-action";
 import FooterSection from "@/components/footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function Home() {
+  
   return (
     <>
       <HeroSection />
