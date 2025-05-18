1999c7ec54d85383b79d5d845bdebcbd31808d12
Onjaniaina754 <o.rabenirina@fulldigits.com>
2025-05-18 08:56:37 +0300

    dd

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,9 +1,19 @@
 import PageLayout from "@/components/PageLayout/PageLayout";
 
-export default function Creation() {
+const Create = () => {
   return (
     <PageLayout>
-      formulaire à insérer ici
+      <Box component="main" className="" sx={{ background: "#000814", color: "white" }}>
+        <GlobalContainer component="section">
+          <Box className="pb-24 pt-12 md:pb-32 lg:pb-56 lg:pt-44" sx={{ margin: "0 auto", maxWidth: "100vw", marginTop: { md: "74px", xs: "74px" } }}>
+            formulaire à insérer ici
+          </Box>
+
+        </GlobalContainer>
+
+      </Box>
+
+
     </PageLayout>
   );
 }
